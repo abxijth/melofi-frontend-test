b621e6fe903cba255c58fe565af1d62b2cad2cb9
abxijth <abhijithrpillai231@gmail.com>
2026-01-07 16:53:42 +0530

    .

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -1,37 +1,54 @@
 import Sidebar from '../../components/Sidebar.jsx'
 import MusicCard from '../../components/MusicCard.jsx'
-import PlaylistCard from '../../components/PlaylistCard.jsx'
-import Button from '../../components/Button.jsx'
 import "./Search.css"
-import mockSongs from '../../data/mockSongs';
-import { Link } from "react-router-dom";
+import mockSongs from '../../data/mockSongs'
+import { Link } from "react-router-dom"
+import { useState } from "react"
 
 const Search = () => {
-	return(
-		<>
-			<div className="wrapper-search">
-				<Sidebar/>
-				<div className="search-bar">
-					<input type="text" placeholder="Search.." />					
-				</div>
 
-				<div className="search-content">
-					{
-							mockSongs.map((song) => (<Link to={`/musicplayer/song/${song.id}`} key={song.id}><MusicCard songBanner={song.songBanner} songName={song.songName} albumName={song.albumName} singer={song.singer}/></Link>))
-					}
-					
+	const [query, setQuery] = useState("")
 
+	const filteredSongs = mockSongs.filter((song) =>
+		song.songName.toLowerCase().includes(query.toLowerCase()) ||
+		song.albumName.toLowerCase().includes(query.toLowerCase()) ||
+		song.singer.toLowerCase().includes(query.toLowerCase())
+	)
 
+	return (
+		<div className="wrapper-search">
+			<Sidebar />
 
-				</div>
+			<div className="search-bar">
+				<input
+					type="text"
+					placeholder="Search.."
+					value={query}
+					onChange={(e) => setQuery(e.target.value)}
+				/>
 			</div>
-		</>
 
-
-
-
-
-		);
+			<div className="search-content">
+				{filteredSongs.length > 0 ? (
+					filteredSongs.map((song) => (
+						<Link
+							to={`/musicplayer/song/${song.id}`}
+							key={song.id}
+						>
+							<MusicCard
+								songBanner={song.songBanner}
+								songName={song.songName}
+								albumName={song.albumName}
+								singer={song.singer}
+							/>
+						</Link>
+					))
+				) : (
+					<p className="no-results">No songs found</p>
+				)}
+			</div>
+		</div>
+	)
 }
 
 export default Search
